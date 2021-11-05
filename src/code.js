280ff9e6182edf0aa16d1417fefa578e86051bb7
ArturoV6 <90590091+ArturoV6@users.noreply.github.com>
2021-11-05 12:26:15 -0700

    carousel part 1

--- src/code.js
+++ src/code.js
@@ -32,6 +32,7 @@ class producto {
         obtainImg.appendChild(fragmento);
     }
     addCart(){
+        console.log(imagenes)
         const overlay = document.getElementById("over");
         let number = this.precio
         const numer = this.precio;
@@ -117,9 +118,75 @@ class producto {
             })
         }
     }
+    addCarousel(){
+        const imagenes = [
+            producto1.img,
+            producto2.img
+        ];
+        
+        let aleatorio = Math.floor(Math.random()*imagenes.length);
+
+        const carouselContainer = document.getElementById("carousel-container");
+        const createDiv1 = document.createElement("div");
+        const fragmento1 = document.createDocumentFragment();
+
+        const createDiv2 = document.createElement("div");
+        const fragmento2 = document.createDocumentFragment();
+
+        const createDiv3 = document.createElement("div");
+        const fragmento3 = document.createDocumentFragment();
+
+        const createDiv4 = document.createElement("div");
+        const fragmento4 = document.createDocumentFragment();
+
+        const createDiv5 = document.createElement("div");
+        const fragmento5 = document.createDocumentFragment();
+
+        const createButton1 = document.createElement("button");
+        const fragmento6 = document.createDocumentFragment();
+
+        const createButton2 = document.createElement("button");
+        const fragmento7 = document.createDocumentFragment();
+
+        const createImg = document.createElement("img");
+        const fragmento8 = document.createDocumentFragment();
+
+        fragmento1.appendChild(createDiv1);
+        carouselContainer.appendChild(fragmento1);
+
+        fragmento2.appendChild(createDiv2);
+        createDiv1.appendChild(fragmento2);
+
+        fragmento3.appendChild(createDiv3);
+        createDiv1.appendChild(fragmento3);
+
+        fragmento4.appendChild(createDiv4);
+        createDiv1.appendChild(fragmento4);
+
+        fragmento5.appendChild(createDiv5);
+        createDiv1.appendChild(fragmento5);
+
+        fragmento6.appendChild(createButton1);
+        createDiv5.appendChild(fragmento6);
+
+        fragmento7.appendChild(createButton2);
+        createDiv5.appendChild(fragmento7);
+
+        fragmento8.appendChild(createImg);
+        createDiv2.appendChild(fragmento8);
+
+        createButton1.innerText = "<";
+        createButton2.innerText = ">";
+
+        createImg.setAttribute("src", imagenes[aleatorio]);
+
+    }
 };
 
 const producto1 = new producto ("The killing joke", 299, "Alan Moore", "/51GgmyZXn4L._SX319_BO1,204,203,200_.png", "Presented for the first time with stark, stunning new coloring by Bolland, Batman: The Killing Joke is Alan Moore's unforgettable meditation on the razor-thin line between sanity and insanity, heroism and villainy, comedy and tragedy. <br> According to the grinning engine of madness and mayhem known as The Joker, that's all that separates the sane from the psychotic. Freed once again from the confines of Arkham Asylum, he's out to prove his deranged point. And he's going to use Gotham City's top cop, Commissioner Jim Gordon, and his brilliant and beautiful daughter Barbara to do it.");
+const producto2 = new producto ("ef", 299, "ujfn", "/51j1lVj-caL._SY291_BO1,204,203,200_QL40_FMwebp_.png");
+
+producto1.addCarousel();
 
 producto1.addProduct();
 
